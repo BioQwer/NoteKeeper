a74845314fa733770b1932bb6d6ef5a1572950fa
Bioqwer <Bioqwer94@gmail.com>
2014-10-27 11:35:43 +0300

    GlassFish4 deploy successfully

---
+++ serverSide/src/main/java/com/bioqwer/serverApp/controller/UserController.java
@@ -0,0 +1,38 @@
+package com.bioqwer.serverApp.controller;
+
+import com.bioqwer.serverApp.model.User;
+import com.bioqwer.serverApp.service.NoteService;
+import com.bioqwer.serverApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Collection;
+
+/**
+ * Created by Antony on 26.10.2014.
+ */
+@Controller
+@RequestMapping(value = "/user")
+public class UserController {
+
+    private final UserService userService;
+    private final NoteService noteService;
+
+    @Autowired
+    public UserController(UserService userService, NoteService noteService) {
+        this.userService = userService;
+        this.noteService = noteService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
+    @ResponseBody
+    public Collection<User> getAuthors() {
+
+        return userService.getAll();
+    }
+
+
+}
