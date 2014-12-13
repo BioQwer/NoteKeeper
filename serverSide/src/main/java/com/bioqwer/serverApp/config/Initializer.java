7b916484345a17d975473e3bedd7f1c83419fa7e
Bioqwer <Bioqwer94@gmail.com>
2014-12-13 16:54:59 +0300

    GF errors

--- serverSide/src/main/java/com/bioqwer/serverApp/config/Initializer.java
+++ serverSide/src/main/java/com/bioqwer/serverApp/config/Initializer.java
@@ -3,8 +3,10 @@ package com.bioqwer.serverApp.config;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -31,5 +33,10 @@ public class Initializer implements WebApplicationInitializer {
                 new DispatcherServlet(ctx));
         servlet.addMapping("/");
         servlet.setLoadOnStartup(1);
+
+        //register filter setting utf-8 encoding on all requests
+        FilterRegistration.Dynamic encoding = servletContext.addFilter("encoding", CharacterEncodingFilter.class);
+        encoding.setInitParameter("encoding", "utf-8");
+        encoding.addMappingForUrlPatterns(null, false, "/*");
     }
 }
